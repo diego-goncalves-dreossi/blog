dc9107b65dc243c15a2032cae5afc59eaf418473
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-07-05 18:36:10 -0300

    Hostsg

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = ['*','https://blog-site-dd.herokuapp.com/']
+ALLOWED_HOSTS = ['https://blog-site-dd.herokuapp.com/']
 # Dominios que a aplicação pode ser acessada
 
 # Application definition
