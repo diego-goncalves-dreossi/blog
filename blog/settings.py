2c3d01e9ddb5ea3a64e67f2ea25bb195210b43be
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-07-05 17:54:15 -0300

    DEGUG

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False,cast=bool)
+DEBUG = True#config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = ['*','https://blog-site-dd.herokuapp.com/']
 # Dominios que a aplicação pode ser acessada
