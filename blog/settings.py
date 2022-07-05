9d4ee8bbc6d32e3ba3307647d1df6255026de35e
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-07-05 15:53:18 -0300

    settings e runtime

--- blog/settings.py
+++ blog/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import os
 from decouple import config
 from dj_database_url import parse as dburl
+import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,3 +164,5 @@ CKEDITOR_CONFIGS = {
 
           },
     }
+
+django_on_heroku.settings(locals())
